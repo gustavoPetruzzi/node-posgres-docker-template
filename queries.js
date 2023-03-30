8629da879acada6fb8e9441055f3dec9c4100367
Gustavo Petruzzi <gustavo.petruzzi@softvision.com>
2023-03-30 01:26:00 -0300

    Add queries and index

--- queries.js
+++ queries.js
@@ -1,11 +1,23 @@
 const Pool = require('pg').Pool;
+
 const pool = new Pool({
-	user: 'user',
-	host: 'localhost',
+	user: process.env.POSTGRES_USER || 'user',
+	host: 'postgres',
 	database: 'db',
+	password: 'pass',
 	port: '5432'
 });
+console.log(pool);
 
-const getUsers = (request, response) => {
-	pool.query("")
+const getUsers = (req, res) => {
+	pool.query('SELECT * FROM users ORDER BY id ASC', (error, results) => {
+		if (error) {
+			throw error
+		}
+		console.log(results);
+		return results;
+	});
 }
+module.exports = {
+	getUsers
+};
