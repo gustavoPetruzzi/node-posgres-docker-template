f98776f81ee6541b2fd70f817e9431f90980f8d4
Gustavo Petruzzi <gustavo.petruzzi@softvision.com>
2023-03-31 03:51:04 -0300

    Add getUsers, getUserById, createUser and routes

--- index.js
+++ index.js
@@ -10,11 +10,50 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 
-app.get('/', (request, response) => {
-  response.json({ info: 'Node.js, Express, and Postgres API sdfsd' })
+app.get('/', (req, res) => {
+  res.json({ info: 'Node.js, Express, and Postgres API sdfsd' })
 });
 
-app.get('/users', db.getUsers);
+app.get('/users', async (req, res) => {
+  try {
+    const users = await db.getUsers();
+    return res.status(200).send(JSON.stringify(users));
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send({
+      error: "Not working"
+    });
+  }
+});
+
+
+app.get('/users/:id', async (req, res) => {
+  try {
+    const id = parseInt(req.params.id);
+    const user = await db.getUserById(id);
+    return res.status(200).send(user);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send({
+      error: "Not working"
+    })
+  }
+});
+
+app.post('/users', async (req, res) => {
+  try {
+    const { name, email } = req.body;
+    const newUserId = await db.createUser(name, email)
+    return res.status(201).send({
+      message: `User created with id ${newUserId}`
+    });
+  } catch (error) {
+    return res.status(500).send({
+      error: "not working"
+    })
+
+  }
+});
 
 
 app.listen(PORT, () => {
--- queries.js
+++ queries.js
@@ -1,3 +1,9 @@
+/**
+ * @typedef User
+ * @prop {number} id user's id
+ * @prop {string} name user's name
+ * @prop {string} email user's email
+ */
 const Pool = require('pg').Pool;
 
 const pool = new Pool({
@@ -7,17 +13,42 @@ const pool = new Pool({
 	password: 'pass',
 	port: '5432'
 });
-console.log(pool);
 
-const getUsers = (req, res) => {
-	pool.query('SELECT * FROM users ORDER BY id ASC', (error, results) => {
-		if (error) {
-			throw error
-		}
-		console.log(results);
-		return results;
-	});
+/**
+ * Get all users
+ * @returns {Promise<User[]>} A promise to users
+ */
+const getUsers = () => {
+	return pool.query('SELECT * FROM users ORDER BY id ASC')
+		.then(results => {
+			return results.rows;
+		});
+}
+
+/**
+ * Get a User by id
+ * @param {number} id - user's id
+ * @returns {Promise<User>} A promise to a user 
+ */
+const getUserById = (id) => {
+	return pool.query('SELECT * FROM users WHERE id = $1', [id])
+		.then(results => {
+			return results.rows;
+		});
+}
+
+/**
+ * Create an user
+ * @param {string} name new user's name 
+ * @param {string} email  new user's email
+ * @returns {number} new user's id
+ */
+const createUser = (name, email) => {
+	return pool.query('INSERT INTO users (name, email) VALUES ($1, $2) RETURNING *', [name, email])
+		.then(results => results.rows[0].id);
 }
 module.exports = {
-	getUsers
+	getUsers,
+	getUserById,
+	createUser
 };
